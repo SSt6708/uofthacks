ea6537c1c188deec058211828c46240b074996ec
chrison <chris.on@mail.utoronto.ca>
2019-01-19 04:53:35 -0500

    Implemented Lot timer system

--- tutorial/src/main/java/parking_lot/LotTimer.java
+++ tutorial/src/main/java/parking_lot/LotTimer.java
@@ -1,4 +1,42 @@
 package parking_lot;
 
+import java.util.List;
+
 public class LotTimer {
+    private boolean entered = false;
+    private long startTime;
+    private long endTime;
+    private long durationTime = endTime - startTime;
+    //gracePeriod in nano seconds.
+    private long gracePeriod = 120 * 1000000000;
+
+    public LotTimer(List<Location> lot_coordinates) {
+        this.entered = true;
+    }
+
+    public boolean OverLotTimer() {
+        if (entered) {
+            startTime = System.nanoTime();
+            endTime = System.nanoTime();
+            while (entered) {
+                endTime = System.nanoTime();
+                if (durationTime >= gracePeriod) {
+                    return true;
+                } else if (!entered && durationTime < gracePeriod) {
+                    ResetTimer();
+                    return false;
+
+                }
+            }
+        }
+        return false;
+    }
+
+    private void ResetTimer() {
+        durationTime = 0;
+    }
+
+    public void setGracePeriod(int seconds) {
+        this.gracePeriod = seconds * 1000000000;
+    }
 }
