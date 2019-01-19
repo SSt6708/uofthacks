30b89a81efc668d2dfb4563a69581932e146a797
huang <tsanyu.huang@mail.utoronto.ca>
2019-01-19 03:59:06 -0500

    Implemented Lot

--- tutorial/src/main/java/parking_lot/Lot.java
+++ tutorial/src/main/java/parking_lot/Lot.java
@@ -1,4 +1,20 @@
 package parking_lot;
 
 public class Lot implements Location{
+    private Coordinate entrance;
+    private Coordinate exit;
+
+    public Lot(Coordinate entrance, Coordinate exit) {
+        this.entrance = entrance;
+        this.exit = exit;
+    }
+
+    public Coordinate getEntrance() {
+        return entrance;
+    }
+
+    public Coordinate getExit() {
+        return exit;
+    }
+
 }
