56e46d1c303b07f0f6846823063bad3c003769d9
huang <tsanyu.huang@mail.utoronto.ca>
2019-01-19 04:13:23 -0500

    Added documentation for LocationManager

--- tutorial/src/main/java/parking_lot/LocationManager.java
+++ tutorial/src/main/java/parking_lot/LocationManager.java
@@ -4,17 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LocationManager {
-
+    /**
+     * List of locations (of bridges, parking lots)
+     */
     private List<Location> locations;
 
+    /**
+     * Creates a new Location manager with no locations initialized
+     */
     public LocationManager() {
         this.locations = new ArrayList<>();
     }
 
+    /**
+     * set the self.locations to locations
+     * @param locations the locations to be set to
+     */
     public void setLocations(List<Location> locations) {
         this.locations = locations;
     }
 
+    /**
+     * get the locations of this LocationManager
+     * @return the locations
+     */
     public List<Location> getLocations() {
         return locations;
     }
