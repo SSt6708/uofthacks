d7da4446bfeb083eec700d8f00728c219002174d
peterzhao <zhiwei.zhao@mail.utoronto.ca>
2019-01-19 04:01:15 -0500

    Added basic functionality of LocationManager.

--- tutorial/src/main/java/parking_lot/LocationManager.java
+++ tutorial/src/main/java/parking_lot/LocationManager.java
@@ -1,4 +1,23 @@
 package parking_lot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocationManager {
+
+    private List<Location> locations;
+
+    public LocationManager() {
+        this.locations = new ArrayList<>();
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+
 }
