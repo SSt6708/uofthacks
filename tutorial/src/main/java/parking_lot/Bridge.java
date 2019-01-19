36e75037754d8902687c7371e3da3ba1b2f0818e
huang <tsanyu.huang@mail.utoronto.ca>
2019-01-19 04:14:53 -0500

    Added documentation for Bridge and Lot

--- tutorial/src/main/java/parking_lot/Bridge.java
+++ tutorial/src/main/java/parking_lot/Bridge.java
@@ -4,6 +4,11 @@ public class Bridge implements Location {
     private Coordinate entrance;
     private Coordinate exit;
 
+    /**
+     * Create a new Bridge with entrance and exit
+     * @param entrance Coordinate of the entrance of the bridge
+     * @param exit Coordinate of the exit of the bridge
+     */
     public Bridge(Coordinate entrance, Coordinate exit) {
         this.entrance = entrance;
         this.exit = exit;
--- tutorial/src/main/java/parking_lot/Lot.java
+++ tutorial/src/main/java/parking_lot/Lot.java
@@ -4,6 +4,11 @@ public class Lot implements Location{
     private Coordinate entrance;
     private Coordinate exit;
 
+    /**
+     * Create a new Lot with entrance and exit
+     * @param entrance Coordinate of the entrance of the lot
+     * @param exit Coordinate of the exit of the lot
+     */
     public Lot(Coordinate entrance, Coordinate exit) {
         this.entrance = entrance;
         this.exit = exit;
