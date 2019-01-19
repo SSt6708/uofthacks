332499675ed26160e43d7fed33fca431b1b4e624
huang <tsanyu.huang@mail.utoronto.ca>
2019-01-19 03:56:32 -0500

    Implemented Location

--- tutorial/src/main/java/parking_lot/Location.java
+++ tutorial/src/main/java/parking_lot/Location.java
@@ -1,4 +1,18 @@
 package parking_lot;
 
 public interface Location {
+
+    /**
+     * Return the Coordinate of the Entrance
+     *
+     * @return the Coordinate of the Entrance
+     */
+    Coordinate getEntrance();
+
+    /**
+     * Return the Coordinate of the Exit
+     *
+     * @return the Coordinate of the Exit
+     */
+    Coordinate getExit();
 }
