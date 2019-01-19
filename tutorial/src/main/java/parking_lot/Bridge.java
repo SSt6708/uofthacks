0f1339fff91947f6d1daec583b547da23cb8a67d
huang <tsanyu.huang@mail.utoronto.ca>
2019-01-19 03:59:27 -0500

    Implemented Bridge

--- tutorial/src/main/java/parking_lot/Bridge.java
+++ tutorial/src/main/java/parking_lot/Bridge.java
@@ -1,4 +1,21 @@
 package parking_lot;
 
 public class Bridge implements Location {
+    private Coordinate entrance;
+    private Coordinate exit;
+
+    public Bridge(Coordinate entrance, Coordinate exit) {
+        this.entrance = entrance;
+        this.exit = exit;
+    }
+
+    @Override
+    public Coordinate getEntrance() {
+        return entrance;
+    }
+
+    @Override
+    public Coordinate getExit() {
+        return exit;
+    }
 }
